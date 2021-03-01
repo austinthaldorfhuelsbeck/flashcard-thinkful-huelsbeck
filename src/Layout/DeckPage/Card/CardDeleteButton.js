b13abb6fd8eae403f33cc2ebab46669fac798a62
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-02-28 18:16:08 -0800

    Add delete card functionality

--- src/Layout/DeckPage/Card/CardDeleteButton.js
+++ src/Layout/DeckPage/Card/CardDeleteButton.js
@@ -3,5 +3,17 @@ import React from "react";
 import { deleteCard } from "../../../utils/api";
 
 export default function CardDeleteButton({ id, setRefresh }) {
-  return <p>Delete</p>;
+  const handleDelete = () => {
+    if (
+      window.confirm("Delete this card? You will not be able to recover it.")
+    ) {
+      deleteCard(id).then(setRefresh);
+    }
+  };
+
+  return (
+    <button type="button" className="btn btn-danger" onClick={handleDelete}>
+      Delete
+    </button>
+  );
 }
