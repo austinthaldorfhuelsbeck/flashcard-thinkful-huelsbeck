e710cbf86861c1f9873397ea97621ddde92f1f75
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-03-02 21:42:52 -0800

    background gradient

--- src/Layout/DeckPage/DeckHeader.js
+++ src/Layout/DeckPage/DeckHeader.js
@@ -6,7 +6,7 @@ import AddCardsButton from "../AddCardsButton";
 
 export default function DeckHeader({ deck }) {
   return (
-    <div className="deck-header pb-4">
+    <div className="deck-header py-4">
       <h3>{deck.name}</h3>
       <p>{deck.description}</p>
       <div className="my-3">
